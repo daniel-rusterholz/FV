436956f55101dd20efc171dbb1d25b20ab21c87d
Daniel <dr@axicode.dk>
2017-07-18 15:32:07 +0200

    Updated comic books controller to use reposetory

--- src/FV/Controllers/FvController.cs
+++ src/FV/Controllers/FvController.cs
@@ -1,4 +1,5 @@
-﻿using FV.Models;
+﻿using FV.Data;
+using FV.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +10,22 @@ namespace FV.Controllers
 {
     public class FvController : Controller
     {
+        private ComicBookReposetory _comicBookReposetory = null;
 
-        public ActionResult Detail()
+        public FvController()
         {
+            _comicBookReposetory = new ComicBookReposetory();
+        }
 
-            var comicBook = new ComicBook()
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script" },
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" },
-                }
-            };
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookReposetory.GetComicBook(id.Value);
 
             return View(comicBook);
         }
